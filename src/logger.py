9d7de817411c38966ff2ab7ac49913edcc17acd2
Mohammed Benaissa <halanmyanime@gmail.com>
2024-11-10 10:19:26 +0100

    Data Ingestion completed

--- src/logger.py
+++ src/logger.py
@@ -2,17 +2,22 @@ import logging
 import os
 from datetime import datetime
 
-
+# Generate log filename based on current timestamp
 LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
-logs_path = os.path.join(os.getcwd(),"logs")
-os.makedirs(logs_path,exist_ok=True)
+logs_dir = os.path.join(os.getcwd(), "logs")
 
-LOG_FILE_PATH = os.path.join(logs_path,LOG_FILE)
+# Create the logs directory if it does not exist
+os.makedirs(logs_dir, exist_ok=True)
 
+# Define the full log file path
+LOG_FILE_PATH = os.path.join(logs_dir, LOG_FILE)
 
+# Configure logging
 logging.basicConfig(
     filename=LOG_FILE_PATH,
     format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
     level=logging.INFO,
 )
 
+# Example log message
+logging.info("Logging setup complete and working!")
